2f9ab5d9c9be951767a518ba9a36a245b6268313
lenovoUser <madsen08@mail.ru>
2018-06-23 21:30:45 +0300

    выравнивание

--- AceStreamPlayer/AceStreamPlayer/Response.cs
+++ AceStreamPlayer/AceStreamPlayer/Response.cs
@@ -1,14 +1,12 @@
-﻿using System;
-namespace AceStreamPlayer
+﻿namespace AceStreamPlayer
 {
     public class Response
     {
-
-		public string is_encrypted { get; set; }
-		public string stat_url { get; set; }
-		public string playback_session_id { get; set; }
-		public string is_live { get; set; }
-		public string playback_url { get; set; }
-		public string command_url { get; set; }
+        public string is_encrypted { get; set; }
+        public string stat_url { get; set; }
+        public string playback_session_id { get; set; }
+        public string is_live { get; set; }
+        public string playback_url { get; set; }
+        public string command_url { get; set; }
     }
 }
--- AceStreamPlayer/AceStreamPlayer/ViewModels/VideoViewModel.cs
+++ AceStreamPlayer/AceStreamPlayer/ViewModels/VideoViewModel.cs
@@ -1,54 +1,57 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.Net.Http;
 using System.Threading.Tasks;
 using FormsVideoLibrary;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
-using Xamarin.Forms.Internals;
 
 namespace AceStreamPlayer
 {
-	public class VideoViewModel : BaseViewModel
+    public class VideoViewModel : BaseViewModel
     {
-		
-		public VideoViewModel(Reference reference, VideoPlayer videoPlayer)
+
+        public VideoViewModel(Reference reference, VideoPlayer videoPlayer)
         {
-			WatchMatch(videoPlayer, reference);
+            WatchMatch(videoPlayer, reference);
         }
-  
 
-		private async void WatchMatch(VideoPlayer player, Reference reference)
-		{
-			 var url =  await GetUrl(reference.ContentId);
-		    player.Source = new UriVideoSource()
-			{
-				Uri = url?.playback_url
-			};
-		}
-        
-		private  async Task<Response>  GetUrl(string contentId)
-		{
-			string url = $"http://192.168.1.37:6878/ace/manifest.m3u8?format=json&id={contentId}";
 
-            HttpClient client = new HttpClient();
-            client.DefaultRequestHeaders.TryAddWithoutValidation("Accept", "application/json");
-            client.DefaultRequestHeaders.TryAddWithoutValidation("User-Agent", "Mozilla/5.0 (Windows NT 6.2; WOW64; rv:19.0) Gecko/20100101 Firefox/19.0");
+        private async void WatchMatch(VideoPlayer player, Reference reference)
+        {
+            var url = await GetUrl(reference.ContentId);
+            player.Source = new UriVideoSource()
+            {
+                Uri = url?.playback_url
+            };
+        }
 
-            client.BaseAddress = new Uri(url);
+        private async Task<Response> GetUrl(string contentId)
+        {
+            try
+            {
+                var uri = $"http://192.168.1.37:6878/ace/manifest.m3u8?format=json&id={contentId}";
 
-			var response = await client.GetAsync(new Uri(url));
-            response.EnsureSuccessStatusCode(); // выброс исключения, если произошла ошибка
+                var client = new HttpClient();
+                client.DefaultRequestHeaders.TryAddWithoutValidation("Accept", "application/json");
+                client.DefaultRequestHeaders.TryAddWithoutValidation("User-Agent", "Mozilla/5.0 (Windows NT 6.2; WOW64; rv:19.0) Gecko/20100101 Firefox/19.0");
 
-			var content = await response.Content.ReadAsStringAsync();
-            JObject json = JObject.Parse(content);
-            var j = json.SelectToken(@"$.response");
-			var ace = JsonConvert.DeserializeObject<Response>(j.ToString());
+                client.BaseAddress = new Uri(uri);
 
-			return ace;
+                var response = await client.GetAsync(new Uri(uri));
+                response.EnsureSuccessStatusCode(); // выброс исключения, если произошла ошибка
 
-		}
+                var content = await response.Content.ReadAsStringAsync();
+                var json = JObject.Parse(content);
+                var j = json.SelectToken(@"$.response");
+                var url = JsonConvert.DeserializeObject<Response>(j.ToString());
+
+                return url;
+            }
+            catch
+            {
+                return null;
+            }
+        }
 
 
     }
