863f30b14118965888ba35989df15d02d88aabca
madsen <>
2018-06-24 00:27:13 +0300

    GetUrl  в base

--- AceStreamPlayer/AceStreamPlayer/ViewModels/BaseViewModel.cs
+++ AceStreamPlayer/AceStreamPlayer/ViewModels/BaseViewModel.cs
@@ -1,21 +1,52 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.ComponentModel;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using Xamarin.Forms;
 
 namespace AceStreamPlayer
 {
-    public abstract class BaseViewModel : INotifyPropertyChanged
-    {
-        public event PropertyChangedEventHandler PropertyChanged;
+	public abstract class BaseViewModel : INotifyPropertyChanged
+	{
+		public event PropertyChangedEventHandler PropertyChanged;
+		public INavigation Navigation { get; set; }
 
-        public virtual void OnPropertyChanged(string propertyName)
-        {
-            PropertyChangedEventHandler handler = PropertyChanged;
-            if (handler != null)
-            {
-                handler.Invoke(this, new PropertyChangedEventArgs(propertyName));
-            }
-        }
+		protected virtual void OnPropertyChanged(string propertyName)
+		{
+			PropertyChangedEventHandler handler = PropertyChanged;
+			if (handler != null)
+			{
+				handler.Invoke(this, new PropertyChangedEventArgs(propertyName));
+			}
+		}
 
-        public INavigation Navigation { get; set; }
-    }
+		protected async Task<T> GetUrl<T>(string uri)
+		{
+			try
+			{
+				var client = new HttpClient();
+				client.DefaultRequestHeaders.TryAddWithoutValidation("Accept", "application/json");
+				client.DefaultRequestHeaders.TryAddWithoutValidation("User-Agent", "Mozilla/5.0 (Windows NT 6.2; WOW64; rv:19.0) Gecko/20100101 Firefox/19.0");
+
+				client.BaseAddress = new Uri(uri);
+
+				var response = await client.GetAsync(new Uri(uri));
+				response.EnsureSuccessStatusCode(); // выброс исключения, если произошла ошибка
+
+				var content = await response.Content.ReadAsStringAsync();
+				var json = JObject.Parse(content).SelectToken($"$.{typeof(T).Name}");
+
+				var url = JsonConvert.DeserializeObject<T>(json.ToString());
+
+				return url;
+			}
+			catch
+			{
+				return default(T);
+			}
+		}
+
+	}
 }
--- AceStreamPlayer/AceStreamPlayer/ViewModels/VideoViewModel.cs
+++ AceStreamPlayer/AceStreamPlayer/ViewModels/VideoViewModel.cs
@@ -1,58 +1,26 @@
-﻿using System;
-using System.Net.Http;
-using System.Threading.Tasks;
-using FormsVideoLibrary;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
+﻿using FormsVideoLibrary;
 
 namespace AceStreamPlayer
 {
-    public class VideoViewModel : BaseViewModel
-    {
+	public class VideoViewModel : BaseViewModel
+	{
 
-        public VideoViewModel(Reference reference, VideoPlayer videoPlayer)
-        {
-            WatchMatch(videoPlayer, reference);
-        }
-
-
-        private async void WatchMatch(VideoPlayer player, Reference reference)
-        {
-            var url = await GetUrl(reference.ContentId);
-            player.Source = new UriVideoSource()
-            {
-                Uri = url?.playback_url
-            };
-        }
-
-        private async Task<Response> GetUrl(string contentId)
-        {
-            try
-            {
-                var uri = $"http://192.168.1.37:6878/ace/manifest.m3u8?format=json&id={contentId}";
-
-                var client = new HttpClient();
-                client.DefaultRequestHeaders.TryAddWithoutValidation("Accept", "application/json");
-                client.DefaultRequestHeaders.TryAddWithoutValidation("User-Agent", "Mozilla/5.0 (Windows NT 6.2; WOW64; rv:19.0) Gecko/20100101 Firefox/19.0");
-
-                client.BaseAddress = new Uri(uri);
-
-                var response = await client.GetAsync(new Uri(uri));
-                response.EnsureSuccessStatusCode(); // выброс исключения, если произошла ошибка
-
-                var content = await response.Content.ReadAsStringAsync();
-				var json = JObject.Parse(content).SelectToken(@"$.response");
-
-                var url = JsonConvert.DeserializeObject<Response>(json.ToString());
-
-                return url;
-            }
-			catch(Exception ex)
-            {
-                return null;
-            }
-        }
+		public VideoViewModel(Reference reference, VideoPlayer videoPlayer)
+		{
+			WatchMatch(videoPlayer, reference);
+		}
 
 
-    }
+		private async void WatchMatch(VideoPlayer player, Reference reference)
+		{
+			var uri = $"http://192.168.1.37:6878/ace/manifest.m3u8?format=json&id={reference.ContentId}";
+
+			var url = await GetUrl<Response>(reference.ContentId);
+			player.Source = new UriVideoSource
+			{
+				Uri = url?.playback_url
+			};
+		}
+
+	}
 }
